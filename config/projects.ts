737ff1ff25ee40d81e403b281be755ebf7af6371
Ronald Lopes <rlopes929@gmail.com>
2026-06-28 00:14:26 -0400

    fix: update project entries and images in portfolio

--- config/projects.ts
+++ config/projects.ts
@@ -1,32 +1,32 @@
 export const projects = [
   {
-    title: 'Bradesco',
-    category: 'Bank | Mobile App',
-    description: 'Bradesco is one of the largest financial groups in America.',
-    image: '/images/projects/bradesco/bradesco.jpg',
-    href: 'projects/bradesco',
+    title: "Liquid DS",
+    category: "Design System",
+    description: "Create consistent experiences cross-platform.",
+    image: "/images/projects/liquid/liquid.jpg",
+    href: "projects/liquid",
   },
   {
-    title: 'Next - NDLS',
-    category: 'Design System',
+    title: "Bradesco",
+    category: "Bank | Mobile App",
+    description: "Bradesco is one of the largest financial groups in America.",
+    image: "/images/projects/bradesco/bradesco.jpg",
+    href: "projects/bradesco",
+  },
+  {
+    title: "Next - NDLS",
+    category: "Design System",
     description: "next is Bradesco's 100% digital and complete account",
-    image: '/images/projects/next/next.jpg',
-    href: 'projects/next',
+    image: "/images/projects/next/next.jpg",
+    href: "projects/next",
   },
   {
-    title: 'Pigz',
-    category: 'Marketplace | Mobile App',
-    description: 'Pigz is a mobile app that helps you to save your money.',
-    image: '/images/projects/pigz/pigz.jpg',
-    href: 'projects/pigz',
+    title: "Pigz",
+    category: "Marketplace | Mobile App",
+    description: "Pigz is a mobile app that helps you to save your money.",
+    image: "/images/projects/pigz/pigz.jpg",
+    href: "projects/pigz",
   },
-  {
-    title: 'Tangerine DS',
-    category: 'Design System',
-    description: 'Create consistent experiences cross-platform.',
-    image: '/images/projects/tangerine/tangerine.jpg',
-    href: 'projects/tangerine',
-  },
-]
+];
 
-export type Projects = typeof projects
+export type Projects = typeof projects;
